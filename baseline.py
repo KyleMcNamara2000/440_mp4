3a4c28dca9848f337e495e4bc5bc70d3885aa931
kylem <kylemcnamara2000@gmail.com>
2020-10-17 19:27:12 -0500

    baseline done

--- baseline.py
+++ baseline.py
@@ -9,5 +9,49 @@ def baseline(train, test):
     output: list of sentences, each sentence is a list of (word,tag) pairs.
             E.g., [[(word1, tag1), (word2, tag2)], [(word3, tag3), (word4, tag4)]]
     '''
-    
-    return []
+    #map word -> map tag -> # times
+    wordDict = {}
+    tagCounts = {} #map tag -> count
+    for sentence in train:
+        for pair in sentence:
+            if pair[1] in tagCounts:
+                tagCounts[pair[1]] += 1
+            else:
+                tagCounts[pair[1]] = 1
+            if pair[0] in wordDict:
+                miniDict = wordDict[pair[0]]
+                if pair[1] in miniDict:
+                    miniDict[pair[1]] += 1
+                else:
+                    miniDict[pair[1]] = 1
+            else:
+                miniDict = {}
+                miniDict[pair[1]] = 1
+                wordDict[pair[0]] = miniDict
+
+    #get max num in dict
+    overallTag = None
+    overallNum = 0
+    for tag, num in tagCounts.items():
+        if num > overallNum:
+            overallTag = tag
+            overallNum = num
+
+    output = [] #list of list of pairs
+    for sentence in test:
+        curr = []
+        for word in sentence:
+            if word in wordDict:
+                miniDict = wordDict[word]
+                maxTag = None
+                maxNum = 0
+                for tag, num in miniDict.items():
+                    if num > maxNum:
+                        maxTag = tag
+                        maxNum = num
+                curr.append((word, maxTag))
+            else:
+                curr.append((word, overallTag))
+        output.append(curr)
+
+    return output
